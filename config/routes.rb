96b7044a641fe6f1bf97b6e6651fac1ad0d2ea4e
usr <mykhaylo.plesha@gmail.com>
2014-06-06 18:30:49 +0300

    get 'graph/show' added

--- config/routes.rb
+++ config/routes.rb
@@ -1,6 +1,6 @@
 Rails.application.routes.draw do
-  #get 'graph/show'
-  root 'graph#show'
+  get 'graph/show'
+  #root '/'
 
   # The priority is based upon order of creation: first created -> highest priority.
   # See how all your routes lay out with "rake routes".
